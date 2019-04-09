4911cd0f748c643a41fcd48bc2296895ee435c52
Thomas Li Fredriksen <thomas.fredriksen@evry.com>
2019-04-09 13:05:44 +0200

    DOC update

--- neo4j_timeseries/time_series_node.py
+++ neo4j_timeseries/time_series_node.py
@@ -23,8 +23,17 @@ class TimeSeriesRel(StructuredRel):
 
 
 class TimeSeries(object):
+    """Time series container-object
+
+    Holds a sequence of time-series nodes
+    """
 
     def __init__(self, dev):
+        """Initialize by reading all nodes connected to a device
+
+        Args:
+            dev: Source device
+        """
 
         self._dev = dev
         self._nodes = []
@@ -32,12 +41,38 @@ class TimeSeries(object):
         self.refresh()
 
     def __len__(self):
+        """
+
+        Returns: Number of nodes connected to source device
+
+        """
         return self._nodes.__len__()
 
     def __getitem__(self, item):
+        """
+
+        Args:
+            item: Item index
+
+        Returns: Item
+
+        """
         return self._nodes.__getitem__(item)
 
     def append(self, value, timestamp=datetime.now(), refresh=True):
+        """Append value to time-series
+
+        Function will call TimeSeriedNode.append then refresh self
+
+        Args:
+            value (float): Value to be appended
+            timestamp (datetime): Timestamp of value
+            refresh (bool): If true then self will refresh, otherwise the returned node will just be appended
+                            to internal list
+
+        Returns:
+            Newly created node
+        """
 
         n = TimeSeriesNode.append(self._dev.devid, value, timestamp)
 
@@ -49,10 +84,16 @@ class TimeSeries(object):
         return n
 
     def refresh(self):
+        """Refresh internal list by querying database for all TimeSeriesNodes connected to source device
+        """
+
+        # Refresh device
 
         if isinstance(self._dev, DeviceConfigNode):
             self._dev.refresh()
         else:
+            # Source device is just an ID. Query DB and inflate node object
+
             query = """
                 match (d: DeviceConfigNode {{devid:'{devid}'}}
                 return d
@@ -63,6 +104,8 @@ class TimeSeries(object):
             res = res[0]
 
             self._dev = DeviceConfigNode.inflate(res[0])
+
+        # Query database for list of nodes connected to source device
 
         query = """
             match (d)-[:EVENT]->(n:TimeSeriesNode) where id(d)={self}
@@ -87,7 +130,6 @@ class TimeSeries(object):
                 raise AssertionError('Bad node: "{}"'.format(str(line[0])))
 
             self._nodes.append(n)
-
 
 
 class TimeSeriesNode(StructuredNode):
