abef3a7746c284f3e374a668671f7f6ffd8ddc85
Thomas Li Fredriksen <thomas.fredriksen@evry.com>
2019-04-09 13:43:16 +0200

    Refactored class TimeSeries to separate file

--- neo4j_timeseries/__init__.py
+++ neo4j_timeseries/__init__.py
@@ -1,2 +1,3 @@
 from .device_config_node import AbstractDeviceConfigNode, DeviceConfigNode
-from .time_series_node import TimeSeriesNode, TimeSeries
+from .time_series_node import TimeSeriesNode
+from .time_series import TimeSeries
